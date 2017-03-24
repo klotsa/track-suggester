7e2aaef6ad899e3d6bbb799e1e0963fc5e6294d0
Anatoliy Klots <Guest@epicodus-7.local>
2017-03-24 14:12:33 -0700

    minor text and style changes

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,12 @@ $(document).ready(function() {
     var q6 = $("select#q6").val();
 
       $("#suggest").show();
+      $("#thanks").show();
 
     if (q2 === "no" && q4 === "no") {
-
       $("#java").show();
       $("#design").show();
       $("#rubyrails").show();
-
     } else if (q1 === "no" && q3 === "no") {
       $("#csharp").show();
       $("#drupal").show();
@@ -23,14 +22,13 @@ $(document).ready(function() {
       $("#java").show();
       $("#rubyrails").show();
       $("#csharp").show();
-    } else if (q1 === "no" && q2 === "no" && q3 === "no" && q4 === "no"&& q5 === "no") {
+    } else if (q1 === "no" && q2 === "no" && q3 === "no" && q4 === "no" && q5 === "no" && q6 === "yes") {
+    // } else if (q1 === "no" && q2 === "no" && q3 === "no" && q4 === "no" && q5 === "no") {
       $("#rejection").show();
     } else {
-
       $("#rubyrails").show();
-
       $("#java").show();
-      // $("#drupal").show();
+      $("#drupal").show();
     }
 
     event.preventDefault();
